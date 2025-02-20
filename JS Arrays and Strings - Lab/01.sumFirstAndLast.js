1c15905b325ccec9a44b456bd7436b9b37426637
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-02-20 19:02:14 +0200

    Task 1 uploaded.
    Another solution added.

--- JS Arrays and Strings - Lab/01.sumFirstAndLast.js
+++ JS Arrays and Strings - Lab/01.sumFirstAndLast.js
@@ -8,3 +8,14 @@ function sumFirstAndLast(inputArray) {
 sumFirstAndLast([20, 30, 40]);
 sumFirstAndLast([10, 17, 22, 33]);
 sumFirstAndLast([11, 58, 69]);
+
+function solve(inputArray) {
+    let first = inputArray.shift();
+    let last = inputArray.pop();
+    let result = first + last;
+    return result;
+}
+
+console.log(solve([20, 30, 40]));
+console.log(solve([10, 17, 22, 33]));
+console.log(solve([11, 58, 69]));
