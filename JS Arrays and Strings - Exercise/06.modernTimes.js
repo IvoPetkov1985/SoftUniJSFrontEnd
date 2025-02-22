9b148997951df32c0ec8964ef779ca4e88977d6b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-02-22 10:58:51 +0200

    Task 6 uploaded.
    Solution without regex added.

--- JS Arrays and Strings - Exercise/06.modernTimes.js
+++ JS Arrays and Strings - Exercise/06.modernTimes.js
@@ -22,3 +22,30 @@ function solve(text) {
 
 solve('Nowadays everyone uses # to tag a #special word in #socialMedia');
 solve('The symbol # is known #variously in English-speaking #regions as the #number sign');
+
+function stringSolve(inputText) {
+    let arrayOfStrings = inputText.split(" ");
+
+    for (let word of arrayOfStrings) {
+
+        if (word.startsWith("#") && word.length > 1) {
+            let isValid = true;
+
+            for (let i = 1; i < word.length; i++) {
+
+                if (!((word[i] >= "a" && word[i] <= "z") ||
+                    (word[i] >= "A" && word[i] <= "Z"))) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) {
+                console.log(word.substring(1));
+            }
+        }
+    }
+}
+
+stringSolve('Nowadays everyone uses # to tag a #special word in #socialMedia');
+stringSolve('The symbol # is known #variously in English-speaking #regions as the #number sign');
