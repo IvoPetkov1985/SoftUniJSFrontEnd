aada2ad1f5793003cceafdb3479669fd97900a34
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-02-22 11:47:32 +0200

    Task 8 uploaded.
    Two more solutions added using regex.

--- JS Arrays and Strings - Exercise/08.pascalCaseSplitter.js
+++ JS Arrays and Strings - Exercise/08.pascalCaseSplitter.js
@@ -27,3 +27,38 @@ function pascalCaseSplitter(text) {
 pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
 pascalCaseSplitter('HoldTheDoor');
 pascalCaseSplitter('ThisIsSoAnnoyingToDo');
+
+function regexSolve(inputText) {
+    let pattern = new RegExp("[A-Z][a-z]*", "g");
+    let matches = inputText.matchAll(pattern);
+    let resultArray = Array.from(matches);
+    let output = resultArray.map(r => r[0]).join(", ");
+    console.log(output);
+}
+
+regexSolve('SplitMeIfYouCanHaHaYouCantOrYouCan');
+regexSolve('HoldTheDoor');
+regexSolve('ThisIsSoAnnoyingToDo');
+
+function solve(text) {
+    let pattern = /[A-Z][a-z]*/g;
+    let resultArray = [];
+
+    while (true) {
+        let result = pattern.exec(text);
+        let word = "";
+
+        if (result) {
+            word = result[0];
+            resultArray.push(word);
+        } else {
+            break;
+        }
+    }
+
+    console.log(resultArray.join(", "));
+}
+
+solve('SplitMeIfYouCanHaHaYouCantOrYouCan');
+solve('HoldTheDoor');
+solve('ThisIsSoAnnoyingToDo');
