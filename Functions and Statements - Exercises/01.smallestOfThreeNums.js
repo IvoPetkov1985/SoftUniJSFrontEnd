2417b983d10c9cf99bb6724454d136085f1fca27
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-02-24 09:21:09 +0200

    Task 1 uploaded.
    Solution with a nested function added.

--- Functions and Statements - Exercises/01.smallestOfThreeNums.js
+++ Functions and Statements - Exercises/01.smallestOfThreeNums.js
@@ -7,3 +7,17 @@ getSmallestNum(2, 5, 3);
 getSmallestNum(600, 342, 123);
 getSmallestNum(25, 21, 4);
 getSmallestNum(2, 2, 2);
+
+function solve(firstNum, secondNum, thirdNum) {
+
+    console.log(getSmallest([firstNum, secondNum, thirdNum]));
+
+    function getSmallest(numbers) {
+        return Math.min(...numbers);
+    }
+}
+
+solve(2, 5, 3);
+solve(600, 342, 123);
+solve(25, 21, 4);
+solve(2, 2, 2);
