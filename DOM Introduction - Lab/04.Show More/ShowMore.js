140dd162f95151711884ce5b7002fd939d8992ab
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-02-27 14:21:58 +0200

    Task 4 uploaded.

---
+++ DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let linkElement = document.querySelector("#more");
+    let extraTextElement = document.querySelector("#text");
+
+    linkElement.style.display = "none";
+    extraTextElement.style.display = "inline";
+}
