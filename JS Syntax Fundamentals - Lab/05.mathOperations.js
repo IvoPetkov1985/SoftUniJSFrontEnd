5731a66fe55338855682f631ba432eac87210bb5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-02-28 13:52:39 +0200

    Task 5 uploaded.
    Solution with object added.

--- JS Syntax Fundamentals - Lab/05.mathOperations.js
+++ JS Syntax Fundamentals - Lab/05.mathOperations.js
@@ -23,3 +23,32 @@ function mathOperations(firstNum, secondNum, oper) {
 mathOperations(5, 6, '+');
 mathOperations(3, 5.5, '*');
 mathOperations(3, 2, '%');
+
+function solve(firstNum, secondNum, operator) {
+    const calculator = {
+        add: (a, b) => a + b,
+        subtract: (a, b) => a - b,
+        multiply: (a, b) => a * b,
+        divide: (a, b) => a / b,
+        divideResidue: (a, b) => a % b,
+        exponentMultiply: (a, b) => a ** b,
+    };
+
+    if (operator === "+") {
+        return calculator.add(firstNum, secondNum);
+    } else if (operator === "-") {
+        return calculator.subtract(firstNum, secondNum);
+    } else if (operator === "*") {
+        return calculator.multiply(firstNum, secondNum);
+    } else if (operator === "/") {
+        return calculator.divide(firstNum, secondNum);
+    } else if (operator === "%") {
+        return calculator.divideResidue(firstNum, secondNum);
+    } else if (operator === "**") {
+        return calculator.exponentMultiply(firstNum, secondNum);
+    }
+}
+
+console.log(solve(5, 6, '+'));
+console.log(solve(3, 5.5, '*'));
+console.log(solve(3, 2, '%'));
