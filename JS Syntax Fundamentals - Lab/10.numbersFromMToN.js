88f17fd15df3bdbd152c1eb96999fc3bc7096ac0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2025-03-01 12:45:14 +0200

    Tasks 9 and 10 uploaded.

---
+++ JS Syntax Fundamentals - Lab/10.numbersFromMToN.js
@@ -0,0 +1,8 @@
+function numbersFromMToN(biggerNum, smallerNum) {
+    for (let i = biggerNum; i >= smallerNum; i--) {
+        console.log(i);
+    }
+}
+
+numbersFromMToN(6, 2);
+numbersFromMToN(4, 1);
